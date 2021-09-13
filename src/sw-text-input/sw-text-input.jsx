5898bedb8e0376333defbdf046c78cdc0a3d52c4
Karol Skolasiński <--global>
2021-09-13 12:10:27 +0200

    fix in index.js, rearrange tag attributes sw-text-input

--- src/sw-text-input/sw-text-input.jsx
+++ src/sw-text-input/sw-text-input.jsx
@@ -2,15 +2,15 @@ import { Component } from 'preact';
 import style from './sw-text-input.style.css';
 
 export default class SwTextInput extends Component {
-    render({ required, name, placeholder }) {
+    render({ name, required, placeholder }) {
         return <>
             <style>{style}</style>
 
             <input id={name}
                    name={name}
                    type="text"
-                   placeholder=" "
                    required={required === 'true'}
+                   placeholder=" "
                    aria-labelledby={placeholder}
             />
             <label htmlFor={name}>{placeholder}</label>
