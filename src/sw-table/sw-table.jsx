2199095c46ce70e0b89b31054be7b34633bb87f1
Szymon <sw@7willows.com>
2021-09-23 17:00:55 +0200

    changes

--- src/sw-table/sw-table.jsx
+++ src/sw-table/sw-table.jsx
@@ -3,25 +3,26 @@ import style from './sw-table.style.css';
 
 export default class SwTable extends Component {
     render({ source, fields }) {
-        if (source && fields && typeof source === 'object' && typeof fields === 'object') {
-            this.setState({
-                source: source,
-                fields: fields
-            });
+        if (source && typeof source === 'string') {
+            source = JSON.parse(source);
         }
 
-        if (this.state.source && this.state.fields) {
+        if (fields && typeof fields === 'string') {
+            fields = JSON.parse(fields);
+        }
+        
+        if (source && fields) {
             return <>
                 <style>{style}</style>
 
                 <table>
                     <thead>
                     <tr>
-                        {this.state.fields?.map(field => <th>{field?.label}</th>)}
+                        {fields?.map(field => <th>{field?.label}</th>)}
                     </tr>
                     </thead>
                     <tbody>
-                    {this.state.source?.map(item => <tr>{this.state.fields?.map(field => field.component
+                    {source?.map(item => <tr>{fields?.map(field => field.component
                             ? <td className="components" nowrap>{field?.component(item)}</td>
                             : <td nowrap>{item[field?.field]}</td>
                         )}
