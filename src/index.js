cc8db7f8d51e3b79d3ae1543ab65b17f051d32d9
Karol Skolasiński <karol.skolasinski@7willows.com>
2021-10-12 11:53:06 +0200

    remove sw-number-input from index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import register from 'preact-custom-element';
 import SwButton from './sw-button/sw-button.jsx';
-import SwNumberInput from './sw-number-input/sw-number-input.jsx';
 import SwTextInput from './sw-text-input/sw-text-input.jsx';
 import SwLoader from './sw-loader/sw-loader.jsx';
 import SwPagination from './sw-pagination/sw-pagination.jsx';
@@ -10,9 +9,6 @@ export * from './sw-utils/sw-flash-message/sw-flash-message.jsx';
 export * from './sw-utils/sw-modal/sw-modal.jsx';
 
 register(SwButton, 'sw-button', ['icon', 'disabled'], { shadow: true });
-register(SwNumberInput, 'sw-number-input',
-    ['name', 'required', 'placeholder', 'disabled', 'value', 'min', 'max', 'step'],
-    { shadow: true });
 register(SwTextInput, 'sw-text-input', ['name', 'required', 'placeholder', 'disabled', 'value'], { shadow: true });
 register(SwLoader, 'sw-loader', [], { shadow: true });
 register(SwPagination, 'sw-pagination', ['current-page', 'number-of-pages'], { shadow: true });
