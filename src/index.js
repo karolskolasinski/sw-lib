7cdb9a2d44ded5829ac2f8778d3ee251738e7390
Karol Skolasiński <karol.skolasinski@7willows.com>
2021-10-19 08:06:36 +0000

    feat: sw-number-input

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import register from 'preact-custom-element';
 import SwButton from './sw-button/sw-button.jsx';
+import SwNumberInput from './sw-number-input/sw-number-input.jsx';
 import SwTextInput from './sw-text-input/sw-text-input.jsx';
 import SwLoader from './sw-loader/sw-loader.jsx';
 import SwPagination from './sw-pagination/sw-pagination.jsx';
@@ -8,6 +9,9 @@ import SwTable from './sw-table/sw-table.jsx';
 export * from './sw-utils/sw-flash-message/sw-flash-message.jsx';
 
 register(SwButton, 'sw-button', ['icon', 'disabled'], { shadow: true });
+register(SwNumberInput, 'sw-number-input',
+    ['name', 'required', 'placeholder', 'disabled', 'value', 'min', 'max', 'step'],
+    { shadow: true });
 register(SwTextInput, 'sw-text-input', ['name', 'required', 'placeholder', 'disabled', 'value'], { shadow: true });
 register(SwLoader, 'sw-loader', [], { shadow: true });
 register(SwPagination, 'sw-pagination', ['current-page', 'number-of-pages'], { shadow: true });
--- src/sw-number-input/sw-number-input.jsx
+++ src/sw-number-input/sw-number-input.jsx
@@ -1,20 +1,22 @@
 import { Component } from 'preact';
 import style from '../common/sw-input/sw-input.style.css';
 
-export default class SwTextInput extends Component {
-    render({ name, required, placeholder, disabled, value }) {
+export default class SwNumberInput extends Component {
+    render({ name, required, placeholder, disabled, value, min, max, step }) {
         return <>
             <style>{style}</style>
-
             <div className="input-wrapper">
                 <input ref={node => this.ref = node}
                        id={name}
                        name={name}
-                       type="text"
+                       type="number"
                        required={required === 'true'}
                        placeholder=" "
                        disabled={disabled}
                        value={value}
+                       min={min && min}
+                       max={max && max}
+                       step={(step) && step === '1' ? 0.1 : step === '2' ? 0.01 : 1}
                        onInput={e => this.ref.getRootNode().host.dispatchEvent(new CustomEvent('changeEvent', {
                                detail: { name: name, value: e.target.value },
                                bubbles: true
