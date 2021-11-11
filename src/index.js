c61a7584393b135ebcbe14d70e832ce74061295d
Karol <karol.skolasinski@7willows.com>
2021-11-11 18:05:23 +0100

    add sw-number to index.js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,22 @@ import SwLoader from './sw-loader/sw-loader.jsx';
 import SwPagination from './sw-pagination/sw-pagination.jsx';
 import SwSelect from './sw-select/sw-select.jsx';
 import SwTable from './sw-table/sw-table.jsx';
+import SwNumberInput from './sw-number-input/sw-number-input';
 
 export * from './sw-utils/sw-flash-message/sw-flash-message.jsx';
 export * from './sw-utils/sw-modal/sw-modal.jsx';
 
 register(SwButton, 'sw-button', ['icon', 'disabled'], { shadow: true });
-register(SwTextInput, 'sw-text-input', ['name', 'required', 'placeholder', 'disabled', 'value'], { shadow: true });
-register(SwDatetimeInput, 'sw-datetime-input', ['name', 'required', 'placeholder', 'disabled', 'value', 'step'], { shadow: true });
+register(SwDatetimeInput,
+    'sw-datetime-input',
+    ['name', 'required', 'placeholder', 'disabled', 'value', 'step'],
+    { shadow: true });
 register(SwLoader, 'sw-loader', [], { shadow: true });
+register(SwNumberInput,
+    'sw-number-input',
+    ['name', 'required', 'placeholder', 'disabled', 'value', 'min', 'max', 'step'],
+    { shadow: true });
 register(SwPagination, 'sw-pagination', ['current-page', 'number-of-pages'], { shadow: true });
 register(SwSelect, 'sw-select', ['config'], { shadow: true });
 register(SwTable, 'sw-table', ['config'], { shadow: true });
+register(SwTextInput, 'sw-text-input', ['name', 'required', 'placeholder', 'disabled', 'value'], { shadow: true });
