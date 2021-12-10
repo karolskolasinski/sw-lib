ef4fe9e54b84ebdcadef8b9feab581cc1089cac2
Karol <karol.skolasinski@7willows.com>
2021-12-10 11:00:04 +0100

    rm Update msg, bug fix

--- src/web-components/sw-pagination/sw-pagination.ts
+++ src/web-components/sw-pagination/sw-pagination.ts
@@ -19,12 +19,7 @@ class AttributeChange {
     }
 }
 
-class Update {
-    constructor(public page: number) {
-    }
-}
-
-type Msg = AttributeChange | Update;
+type Msg = AttributeChange;
 
 const propTypes = {
     currentPage: Number,
@@ -87,13 +82,6 @@ function update(state: State, msg: Msg): [State, stateMgr.Cmd<Msg>] | undefined 
         console.error('impossible state');
         return [state, null];
     }
-
-    if (msg instanceof Update) {
-        return [state, new CustomEvent('update', {
-            bubbles: true,
-            detail: {page: msg.page},
-        })];
-    }
 }
 
 function view(state: State): stateMgr.View<Msg> {
@@ -101,6 +89,7 @@ function view(state: State): stateMgr.View<Msg> {
         return ['sw-loader'];
     }
 
+    console.log(state);
 
     return ['.wrapper', [
         ['style', style],
@@ -110,7 +99,7 @@ function view(state: State): stateMgr.View<Msg> {
         state.numberOfPages > 1 && state.currentPage !== 1 && generateTheATag(state, 1, '1', null),
 
 
-        (state.numberOfPages > 7 && state.currentPage > 4) || (state.numberOfPages === 7 && state.currentPage > 5)
+        ((state.numberOfPages >= 7 && state.currentPage > 4) || (state.numberOfPages === 7 && state.currentPage > 5))
         && ['span.caption', '...'],
 
 
@@ -155,8 +144,7 @@ function view(state: State): stateMgr.View<Msg> {
 function generateTheATag(state, page, innerText, className) {
     return ['a', {
         href: router.getRouteUrl(state.routeName, {page}),
-        className: className && className,
-        onclick: new Update(page)
+        className: className && className
     }, innerText];
 }
 
