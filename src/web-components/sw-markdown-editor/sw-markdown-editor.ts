ea364afcb5984fa0519a7da56139620df4de2337
Szymon <sw@7willows.com>
2022-01-04 11:08:38 +0100

    temp work on sw-markdown-editor

--- src/web-components/sw-markdown-editor/sw-markdown-editor.ts
+++ src/web-components/sw-markdown-editor/sw-markdown-editor.ts
@@ -30,22 +30,23 @@ stm.component({
 });
 
 function view(state: State): stm.View<Msg> {
-    return v('.sw-input.sw-text-input',
-        v.style(style),
-        v('.input-wrapper',
-            v.textarea({
-                type: 'number',
-                required: state.required,
-                placeholder: ' ',
-                disabled: state.disabled,
-                value: state.value,
-                min: state.min ?? '',
-                max: state.max ?? '',
-                step: state.step ?? '',
-                ariaLabelledby: state.label,
-                oninput: (event: any) => ['Input', event.target.value, event.target.validationMessage]
-            }],
-                state.showLabel && ['label', { htmlFor: state.name }, state.label]
-        ]]
-    ]];
+    return v('textarea');
+    // return v('.sw-input.sw-text-input',
+    //     v.style(style),
+    //     v('.input-wrapper',
+    //         v.textarea({
+    //             type: 'number',
+    //             required: state.required,
+    //             placeholder: ' ',
+    //             disabled: state.disabled,
+    //             value: state.value,
+    //             min: state.min ?? '',
+    //             max: state.max ?? '',
+    //             step: state.step ?? '',
+    //             ariaLabelledby: state.label,
+    //             oninput: (event: any) => ['Input', event.target.value, event.target.validationMessage]
+    //         }],
+    //             state.showLabel && ['label', { htmlFor: state.name }, state.label]
+    //     ]]
+    // ]];
 }
